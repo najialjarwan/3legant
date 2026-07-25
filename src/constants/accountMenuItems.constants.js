15475b662ee9a1c10826e4754b785d239770e063
najijrwan <najijrwanyt@gmail.com>
2026-07-25 22:41:15 +0300

    refactor(account): extract accountMenuItems constant to constants folder

---
+++ src/constants/accountMenuItems.constants.js
@@ -0,0 +1,7 @@
+export const ACCOUNT_MENU_ITEMS = {
+    ACCOUNT: 0,
+    ADDRESS: 1,
+    ORDERS: 2,
+    WISHLIST: 3,
+    LOGOUT: 4,
+};
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
 export { GRID_MODES } from './gridModes.constants';
 export { CART_STEPS } from './cart.constants';
+export { ACCOUNT_MENU_ITEMS } from './accountMenuItems.constants';
