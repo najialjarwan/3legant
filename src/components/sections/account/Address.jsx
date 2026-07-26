b951aec73d90b5c696764714f06651602d22b525
najijrwan <najijrwanyt@gmail.com>
2026-07-26 07:36:25 +0300

    feat(accout): create address component section

---
+++ src/components/sections/account/Address.jsx
@@ -0,0 +1,47 @@
+import { Icon } from '@ui';
+
+const BILLING_ADDRESS = [
+    "Sofia Havertz",
+    "(+1) 234 567 890",
+    "345 Long Island, NewYork, United States",
+];
+
+const SHIPPING_ADDRESS = [
+    "Sofia Havertz",
+    "(+1) 234 567 890",
+    "345 Long Island, NewYork, United States",
+];
+
+const Address = () => {
+    return (
+        <div className='w-full pr-[72px]'>
+            <header className="mb-[19px] text-black body-1-semi capitalize">address</header>
+            <div className="w-full flex flex-col 2xl:flex-row gap-[23px]">
+                <AddressContainer header="billing address" info={BILLING_ADDRESS} />
+                <AddressContainer header="shipping address" info={SHIPPING_ADDRESS} />
+            </div>
+        </div>
+    )
+}
+
+export default Address;
+
+const AddressContainer = ({ header, info }) => (
+    <div className="w-full p-4 border border-n4100 rounded-lg">
+        <header className="w-full mb-2 flex items-center justify-between body-2-semi capitalize">
+            <span className="text-black">{header}</span>
+            <button className="flex items-center gap-1 text-n4100">
+                <Icon name="Pen"/>
+                <span>Edit</span>
+            </button>
+        </header>
+        <ul className="flex flex-col gap-1 text-black caption-1">
+            {info.map((info, i) => (
+                <li key={i}>
+                    {info}
+                </li>
+            ))}
+        </ul>
+    </div>
+);
+
