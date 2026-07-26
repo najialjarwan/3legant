76e96101cab54ecb72f8228d2e01e951622fba0e
najijrwan <najijrwanyt@gmail.com>
2026-07-26 22:06:31 +0300

    style(ui): add hover state styles for AddToCartBtn component

--- src/components/ui/buttons/AddToCartBtn.jsx
+++ src/components/ui/buttons/AddToCartBtn.jsx
@@ -1,6 +1,13 @@
 const AddToCartBtn = ({ className = '' }) => {
     return (
-        <button className={`rounded-lg bg-n7100 shadow-1 ${className}`}>
+        <button
+            className={`
+            bg-n7100 border border-n7100 rounded-lg shadow-1
+            hover:text-n7100 hover:bg-transparent
+            transition-all duration-150 ease-linear
+            ${className}
+            `}
+        >
             Add to cart
         </button>
     );
