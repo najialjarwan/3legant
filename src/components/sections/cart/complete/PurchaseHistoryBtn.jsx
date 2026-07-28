e8e3462b55f4b9cfd5de9f086015157c9b23a7da
najijrwan <najijrwanyt@gmail.com>
2026-07-28 07:34:24 +0300

    feat(cart): change the button to a link and add a state and a page direct

--- src/components/sections/cart/complete/PurchaseHistoryBtn.jsx
+++ src/components/sections/cart/complete/PurchaseHistoryBtn.jsx
@@ -1,15 +1,20 @@
+import { Link } from "react-router-dom";
+import { ACCOUNT_MENU_ITEMS } from "@constants";
+
 const PurchaseHistoryBtn = () => (
-    <button
+    <Link
+        to="/account"
+        state={{ activeTab: ACCOUNT_MENU_ITEMS.ORDERS }}
         className="
         w-full 2xl:w-auto px-10 py-3
-        text-white btn-s
+        text-white btn-s text-center
         bg-n7100 border border-transparent rounded-[80px]
         hover:text-n7100 hover:bg-transparent hover:border-n7100
         transition-all duration-150 ease-linear
         "
     >
         Purchase history
-    </button>
+    </Link>
 );
 
 export default PurchaseHistoryBtn;
