cfe4660be4dc472668a00675861666e7869bc0f0
najijrwan <najijrwanyt@gmail.com>
2026-07-29 15:32:17 +0300

    feat(data): add new blog article entry

--- src/data/blog/blogArticlesList.js
+++ src/data/blog/blogArticlesList.js
@@ -1,5 +1,36 @@
 export const ARTICLES_LIST = [
     {
+        slug: "How-to-make-a-busy-bathroom-a-place-to-relax",
+        img: "src/assets/images/article-image-10.png",
+        title: "How to make a busy bathroom a place to relax",
+        date: "October 16, 2023",
+        excerpt: "Your bathroom serves a string of busy functions on a daily basis. See how you can make all of them work, and still have room for comfort and relaxation.",
+        isFeatured: true,
+        author: "Henrik Annemark",
+        content: [
+            {
+                header: "A cleaning hub with built-in ventilation",
+                text: "Use a rod and a shower curtain to create a complement to your cleaning cupboard. Unsightly equipment is stored out of sight yet accessibly close – while the air flow helps dry any dampness.",
+            },
+            {
+                header: "Storage with a calming effect",
+                text: "Having a lot to store doesn’t mean it all has to go in a cupboard. Many bathroom items are better kept out in the open – either to be close at hand or are nice to look at. Add a plant or two to set a calm mood for the entire room (and they’ll thrive in the humid air).",
+            },
+            {
+                header: 'Kit your clutter for easy access',
+                text: 'Even if you have a cabinet ready to swallow the clutter, it’s worth resisting a little. Let containers hold kits for different activities – home spa, make-up, personal hygiene – to bring out or put back at a moment’s notice.',
+            },
+            {
+                header: 'An ecosystem of towels',
+                text: 'Racks or hooks that allow air to circulate around each towel prolong their freshness. They dry quick and the need for frequent washing is minimized.',
+            },
+            {
+                header: 'Make your mop disappear',
+                text: 'Having your cleaning tools organized makes them easier to both use and return to. When they’re not needed, close the curtain and feel the peace of mind it brings.',
+            },
+        ],
+    },
+    {
         slug: "7-ways-to-decor-your-home-like-a-professional",
         img: "src/assets/images/article-image-1.png",
         title: "7 ways to decor your home like a professional",
