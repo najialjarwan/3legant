cbcb1c4f1e4572760d7840136c3b364bfc9f8963
najijrwan <najijrwanyt@gmail.com>
2026-07-30 15:23:24 +0300

    refactor(pages): replace Blog component with ArticlesRec component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,6 @@
-import { HeroSlider, BannerGrid, NewArrivals, Values, Banner, Blog } from '@home';
+import { HeroSlider, BannerGrid, NewArrivals, Values, Banner } from '@home';
+import { ArticlesRec } from '@ui';
+import { ARTICLES_LIST } from '@data';
 
 const HomePage = () => {
     return (
@@ -13,7 +15,15 @@ const HomePage = () => {
 
             <Banner />
 
-            <Blog title="Articles" />
+            <ArticlesRec
+                titleVariant="default"
+                title="articles"
+                btnLabel="more articles"
+                labelClass="text-n7100"
+                iconClass="text-n7100"
+                ARTICLES_LIST={ARTICLES_LIST.slice(1, 4)}
+                withReadMore={true}
+            />
         </>
     );
 };
