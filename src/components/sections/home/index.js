ef5d2febbd38454b4c38b6b76795bb31043ceb10
najijrwan <najijrwanyt@gmail.com>
2026-07-30 15:20:56 +0300

    refactor(home): delete Blog component and unexport it

--- src/components/sections/home/index.js
+++ src/components/sections/home/index.js
@@ -3,4 +3,3 @@ export { default as BannerGrid } from './BannerGrid';
 export { default as NewArrivals } from './NewArrivals';
 export { default as Values } from './Values';
 export { default as Banner } from './Banner';
-export { default as Blog } from './Blog';
