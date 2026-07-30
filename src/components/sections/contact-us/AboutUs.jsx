f49baf3538448aea6511d2549b4f0903d60834d6
najijrwan <najijrwanyt@gmail.com>
2026-07-30 18:42:48 +0300

    style(contact-us): adjuct styles to match the design

--- src/components/sections/contact-us/AboutUs.jsx
+++ src/components/sections/contact-us/AboutUs.jsx
@@ -12,22 +12,23 @@ const AboutUs = () => {
             </div>
             <div
                 className='
-                w-full h-[311px] 2xl:h-[413px]
+                w-full h-[311px] 2xl:h-[413px] px-4 2xl:pl-18 2xl:pr-0 2xl:mb-12
                 flex flex-col justify-center gap-6
+                bg-n2100
                 '
             >
                 <div>
                     <h6 className="mb-4 text-black-900 h7 2xl:h4 capitalize">
                         about us
                     </h6>
-                    <p className='text-n7100 caption-1 2xl:body-2'>
-                        3legant is a gift & decorations store based in HCMC, Vietnam. Est since 2019.
+                    <p className='max-w-[452px] text-n7100 caption-1 2xl:body-2'>
+                        3legant is a gift & decorations store based in HCMC, Vietnam. Est since 2019.<br />
                         Our customer service is always prepared to support you 24/7
                     </p>
                 </div>
                 <MoreActionBtn
                     label="shop now"
-                    labelClass="text-black-900 btn-s"
+                    labelClass="text-black-900 btn-s!"
                     iconSpanClass="size-5"
                     iconClass="text-black-900"
                 />
