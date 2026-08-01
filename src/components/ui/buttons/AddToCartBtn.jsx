b6f7d26230da1e3b343ccfcbfbc1f71b068c6450
najijrwan <najijrwanyt@gmail.com>
2026-08-01 22:55:48 +0300

    style(ui): remove utility rounded lg

--- src/components/ui/buttons/AddToCartBtn.jsx
+++ src/components/ui/buttons/AddToCartBtn.jsx
@@ -2,7 +2,7 @@ const AddToCartBtn = ({ className = '' }) => {
     return (
         <button
             className={`
-            bg-n7100 border border-n7100 rounded-lg shadow-1
+            bg-n7100 border border-n7100 shadow-1
             hover:text-n7100 hover:bg-transparent
             transition-all duration-150 ease-linear
             ${className}
