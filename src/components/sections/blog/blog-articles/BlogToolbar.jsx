b7e061df02da739e41089422c3b31abfa5ad53cc
najijrwan <najijrwanyt@gmail.com>
2026-08-02 23:31:18 +0300

    refactor(blog): rename activeSelector to gridMode

--- src/components/sections/blog/blog-articles/BlogToolbar.jsx
+++ src/components/sections/blog/blog-articles/BlogToolbar.jsx
@@ -2,7 +2,7 @@ import { useBreakpoint } from '@hooks';
 import { BLOG_MENU } from '@data';
 import { Dropdown, ViewSelectors } from '@ui';
 
-const BlogToolbar = ({ activeItem, setActiveItem, activeSelector, onClick, }) => {
+const BlogToolbar = ({ activeItem, setActiveItem, gridMode, onClick, }) => {
     const { isMobile } = useBreakpoint();
 
     if (isMobile) {
@@ -37,7 +37,7 @@ const BlogToolbar = ({ activeItem, setActiveItem, activeSelector, onClick, }) =>
                     ))}
                 </ul>
                 <ViewSelectors
-                    activeSelector={activeSelector}
+                    gridMode={gridMode}
                     onClick={onClick}
                 />
             </div>
