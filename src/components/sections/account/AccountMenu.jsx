ecaf8ff40f000247588cf944138602119cf3418b
najijrwan <najijrwanyt@gmail.com>
2026-08-03 20:28:28 +0300

    style(account): add h-fit to the parent component so the height doesn't extend on content change

--- src/components/sections/account/AccountMenu.jsx
+++ src/components/sections/account/AccountMenu.jsx
@@ -6,7 +6,7 @@ const AccountMenu = ({ activeItem, setActiveItem }) => {
     const { isMobile } = useBreakpoint();
 
     return (
-        <div className='px-4 py-10 bg-n2100 rounded-lg'>
+        <div className='h-fit px-4 py-10 bg-n2100 rounded-lg'>
             <div className='relative size-[82px] mx-auto rounded-full'>
                 <img src="src/assets/images/avatar sofia havertz.png" alt="user profile" />
                 <div
