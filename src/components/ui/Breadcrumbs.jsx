40dc7146f8c8dcb6cd2b0e2955ccd8edc912c38b
najijrwan <najijrwanyt@gmail.com>
2026-08-04 22:59:08 +0300

    feat(ui): adjust Breadcrumbs to accept a className and add a conditioned labelClass

--- src/components/ui/Breadcrumbs.jsx
+++ src/components/ui/Breadcrumbs.jsx
@@ -1,23 +1,40 @@
 import { Link } from 'react-router-dom';
 import { Icon } from '@ui';
 
-const Breadcrumbs = ({ items }) => (
-    <nav aria-label="Breadcrumb">
-        <ol className="flex items-center gap-4">
-            {items.map((item) => (
-                <li key={item.label}>
-                    {item.href ? (
-                        <Link to={item.href} className="flex items-center gap-1 text-black-600">
-                            <span className='btn-xs capitalize'>{item.label}</span>
-                            <Icon name="ChevronRight" spanClassName="size-3" iconClassName="w-[5px] h-[8px]"/>
-                        </Link>
-                    ) : (
-                        <span className='block text-black-900 btn-xs'>{item.label}</span>
-                    )}
-                </li>
-            ))}
-        </ol>
-    </nav>
-);
+const Breadcrumbs = ({ items, className = 'gap-4' }) => {
+    const labelClass =
+        items.length >= 4
+            ? 'text-[12px]/[20px] font-inter font-medium 2xl:btn-xs'
+            : 'btn-xs';
+
+    return (
+        <nav aria-label="Breadcrumb">
+            <ol className={`flex items-center ${className}`}>
+                {items.map((item) => (
+                    <li key={item.label}>
+                        {item.href ? (
+                            <Link
+                                to={item.href}
+                                state={item.state}
+                                className="flex items-center gap-1 text-black-600"
+                            >
+                                <span className={`capitalize ${labelClass}`}>
+                                    {item.label}
+                                </span>
+                                <Icon
+                                    name="ChevronRight"
+                                    spanClassName="size-3"
+                                    iconClassName="w-[5px] h-[8px]"
+                                />
+                            </Link>
+                        ) : (
+                            <span className={`block text-black-900 btn-xs ${labelClass}`}>{item.label}</span>
+                        )}
+                    </li>
+                ))}
+            </ol>
+        </nav>
+    );
+}
 
 export default Breadcrumbs;
