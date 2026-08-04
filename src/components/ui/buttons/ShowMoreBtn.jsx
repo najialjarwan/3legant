5435d9a1e4925a49e50db45025195c2b4a34df26
najijrwan <najijrwanyt@gmail.com>
2026-08-04 23:07:48 +0300

    feat(ui): add disabled attribute to ShowMoreBtn and use it to add styles

--- src/components/ui/buttons/ShowMoreBtn.jsx
+++ src/components/ui/buttons/ShowMoreBtn.jsx
@@ -1,12 +1,15 @@
-const ShowMoreBtn = ({ onClick, label }) => (
+const ShowMoreBtn = ({ disabled = false, onClick, label }) => (
     <button
+        disabled={disabled}
         onClick={onClick}
-        className="
-        px-10 py-1.5
+        className={`
+        w-fit px-10 py-1.5
         text-n7100 btn-s capitalize
         border border-n7100 rounded-[80px]
         hover:bg-n7100 hover:text-white
-        transition-colors duration-300 ease-out"
+        transition-colors duration-300 ease-out
+        ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'}
+        `}
     >
         {label}
     </button>
