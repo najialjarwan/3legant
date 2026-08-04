2f558f1391a9047eceb61b1ff60d956a7cc9dc59
najijrwan <najijrwanyt@gmail.com>
2026-08-04 23:05:10 +0300

    style(proudct): add conditioned styles for the section to match the design

--- src/components/sections/product/ProductLoop/ProductLoop.jsx
+++ src/components/sections/product/ProductLoop/ProductLoop.jsx
@@ -4,9 +4,11 @@ const ProductLoop = ({ layout }) => {
 
     return (
         <section
-            className="
-                px-8 2xl:px-40
-                flex flex-col 2xl:flex-row 2xl:justify-between"
+            className={`
+                px-8 2xl:px-40  
+                flex flex-col 2xl:flex-row 2xl:justify-between
+                ${layout.showRecommendations ? 'mb-20 2xl:mb-10' : ''}
+            `}
         >
             <ProductMedia layout={layout} />
 
