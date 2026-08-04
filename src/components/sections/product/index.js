39cd6dcfe986a3bf5d04baa99b1dadbfe798b1eb
najijrwan <najijrwanyt@gmail.com>
2026-08-04 23:10:03 +0300

    fix(proudct): unexport ProudctBreadcrumbs

--- src/components/sections/product/index.js
+++ src/components/sections/product/index.js
@@ -4,7 +4,6 @@ export * from './product-context/ProductContext';
 export { default as ProductLayout } from './product-layout/ProductLayout';
 
 
-export { default as ProductBreadcrumbs } from './ProductLoop/ProductBreadcrumbs';
 export { default as ProductLoop } from './ProductLoop/ProductLoop';
 export { default as ProductInfo } from './ProductLoop/product-info/ProductInfo';
 export { default as ProductMedia } from './ProductLoop/product-media/ProductMedia';
