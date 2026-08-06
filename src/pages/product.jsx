bf07525e4a28f5f8cefc8572fcbf05113194023e
najijrwan <najijrwanyt@gmail.com>
2026-08-06 23:34:22 +0300

    refactor(pages): remove Breadcrumbs component

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,25 +1,9 @@
-import { SHOP_CATEGORIES_ITEMS } from '@constants';
-import { Breadcrumbs } from '@ui';
 import { ProductLayout } from '@product';
 
 const ProductPage = () => {
 
     return (
         <>
-            <Breadcrumbs
-                items={[
-                    { label: "home", href: '/' },
-                    { label: 'shop', href: '/shop' },
-                    {
-                        label: 'living room',
-                        href: '/shop',
-                        state: { category: SHOP_CATEGORIES_ITEMS.LIVING_ROOM },
-                    },
-                    { label: 'Product' }
-                ]}
-                className='px-8 2xl:px-40 py-4 gap-2 2xl:gap-4'
-            />
-
             <ProductLayout />
         </>
     );
