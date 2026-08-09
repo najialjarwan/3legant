d0e52ba90bbe0d05145090ea64329d800c314204
najijrwan <najijrwanyt@gmail.com>
2026-08-09 18:51:36 +0300

    feat(utils): adjust utitlity to return correct discounted price

--- src/utils/finalPrice.js
+++ src/utils/finalPrice.js
@@ -1,11 +1,11 @@
-export const finalPrice = (price, discountPercentage) => {
+export const finalPrice = (price, discountPercentage = 0) => {
     const discountPrice = price * (discountPercentage / 100);
     let result = 0;
     if (discountPrice % 10 === 0)
-        result = discountPrice - 1;
+        result = price - discountPrice - 1;
     else if (discountPrice % 5 === 0 || discountPrice % 2 === 0)
-        result = discountPrice - 0.01
+        result = price - discountPrice - 0.01
     else
-        result = discountPrice;
+        result = price - discountPrice;
     return result;
 }
