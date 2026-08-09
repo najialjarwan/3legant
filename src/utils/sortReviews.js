606569e847005cd557682606bbc76d541dbbb39e
najijrwan <najijrwanyt@gmail.com>
2026-08-09 10:53:19 +0300

    chore(utils): remove a console.log

--- src/utils/sortReviews.js
+++ src/utils/sortReviews.js
@@ -4,7 +4,6 @@ export const sortReviews = (reviews, filter) => {
     return [...reviews].sort((a, b) => {
         const dateA = new Date(a.datePosted || 0); // fallback for missing
         const dateB = new Date(b.datePosted || 0);
-        console.log(dateA)
 
         if (filter === REVIEWS_FILTER.NEWEST) {
             return dateB - dateA; // newest first
