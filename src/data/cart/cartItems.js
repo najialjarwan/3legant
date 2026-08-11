a66431da90b3f80cdbbd7f9d5fe51435e6a36a67
najijrwan <najijrwanyt@gmail.com>
2026-08-11 18:37:44 +0300

    refactor(data): adjust CART_ITEMS properties and values to be driven from PRODUCTS

--- src/data/cart/cartItems.js
+++ src/data/cart/cartItems.js
@@ -1,23 +1,29 @@
+import { PRODUCTS } from '@data';
+import { finalPrice } from '@utils';
+
 export const CART_ITEMS = [
     {
-        image: 'src/assets/images/product image 1 black.png',
-        name: 'Tray Table',
-        color: 'black',
+        id: PRODUCTS.TRAY_TABLE.id,
+        label: PRODUCTS.TRAY_TABLE.label,
+        image: PRODUCTS.TRAY_TABLE.variants.BLACK.image,
+        color: PRODUCTS.TRAY_TABLE.variants.BLACK.color,
         quantity: 2,
-        price: 19,
+        price: finalPrice(PRODUCTS.TRAY_TABLE.price, PRODUCTS.TRAY_TABLE.discountPercentage),
     },
     {
-        image: 'src/assets/images/product image 1 red.png',
-        name: 'Tray Table 2',
-        color: 'red',
+        id: PRODUCTS.TRAY_TABLE.id,
+        label: PRODUCTS.TRAY_TABLE.label,
+        image: PRODUCTS.TRAY_TABLE.variants.RED.image,
+        color: PRODUCTS.TRAY_TABLE.variants.RED.color,
         quantity: 2,
-        price: 19,
+        price: finalPrice(PRODUCTS.TRAY_TABLE.price, PRODUCTS.TRAY_TABLE.discountPercentage),
     },
     {
-        image: 'src/assets/images/product image 3.png',
-        name: 'Table Lamp',
-        color: 'Gold',
+        id: PRODUCTS.TABLE_LAMP.id,
+        label: PRODUCTS.TABLE_LAMP.label,
+        image: PRODUCTS.TABLE_LAMP.variants.GOLD.image,
+        color: PRODUCTS.TABLE_LAMP.variants.GOLD.color,
         quantity: 1,
-        price: 39,
+        price: finalPrice(PRODUCTS.TABLE_LAMP.price, PRODUCTS.TABLE_LAMP.discountPercentage),
     },
 ]
