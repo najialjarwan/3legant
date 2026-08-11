47bcffaf32f019654b5865d4782ddd14db542dcb
najijrwan <najijrwanyt@gmail.com>
2026-08-11 21:00:50 +0300

    feat(data): add a new property taxRate for some products in PRODUCTS and in CART_ITEMS

--- src/data/cart/cartItems.js
+++ src/data/cart/cartItems.js
@@ -9,6 +9,7 @@ export const CART_ITEMS = [
         color: PRODUCTS.TRAY_TABLE.variants.BLACK.color,
         quantity: 2,
         price: finalPrice(PRODUCTS.TRAY_TABLE.price, PRODUCTS.TRAY_TABLE.discountPercentage),
+        taxRate: PRODUCTS.TRAY_TABLE.taxRate,
     },
     {
         id: PRODUCTS.TRAY_TABLE.id,
@@ -17,6 +18,7 @@ export const CART_ITEMS = [
         color: PRODUCTS.TRAY_TABLE.variants.RED.color,
         quantity: 2,
         price: finalPrice(PRODUCTS.TRAY_TABLE.price, PRODUCTS.TRAY_TABLE.discountPercentage),
+        taxRate: PRODUCTS.TRAY_TABLE.taxRate,
     },
     {
         id: PRODUCTS.TABLE_LAMP.id,
@@ -25,5 +27,6 @@ export const CART_ITEMS = [
         color: PRODUCTS.TABLE_LAMP.variants.GOLD.color,
         quantity: 1,
         price: finalPrice(PRODUCTS.TABLE_LAMP.price, PRODUCTS.TABLE_LAMP.discountPercentage),
+        taxRate: PRODUCTS.TABLE_LAMP.taxRate,
     },
 ]
