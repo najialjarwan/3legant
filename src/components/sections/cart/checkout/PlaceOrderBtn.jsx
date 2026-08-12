bef2b9a07aee78d7fc365796d17dbd702c06dd71
najijrwan <najijrwanyt@gmail.com>
2026-08-12 21:00:09 +0300

    style(cart): capitalize button label

--- src/components/sections/cart/checkout/PlaceOrderBtn.jsx
+++ src/components/sections/cart/checkout/PlaceOrderBtn.jsx
@@ -4,7 +4,7 @@ const PlaceOrderBtn = ({ onClick }) => {
             onClick={onClick}
             className="
             w-full px-10 py-3
-            text-white btn-s
+            text-white btn-s capitalize
             bg-n7100 border border-transparent rounded-lg
             hover:bg-transparent hover:text-n7100 hover:border hover:border-n7100
             transition-all duration-300 ease-in-out"
