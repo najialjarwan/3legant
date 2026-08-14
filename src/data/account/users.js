5753e03f8cd019096ddb3061f55e2fb24da5caca
najijrwan <najijrwanyt@gmail.com>
2026-08-14 15:40:28 +0300

    fix(data): update images paths

--- src/data/account/users.js
+++ src/data/account/users.js
@@ -1,62 +1,62 @@
 export const USERS = [
     {
         userName: 'Sofia Havertz',
-        avatar: '/users-avatar/sofia havertz.png',
+        avatar: '/images/users-avatar/sofia havertz.png',
     },
     {
         userName: 'Nicola Jensen',
-        avatar: '/users-avatar/nicolas jensen.png',
+        avatar: '/images/users-avatar/nicolas jensen.png',
     },
     {
         userName: 'Ashley Lee',
-        avatar: '/users-avatar/ashley lee.png',
+        avatar: '/images/users-avatar/ashley lee.png',
     },
     {
         userName: 'Jessica Joe',
-        avatar: '/users-avatar/jessica joe.png',
+        avatar: '/images/users-avatar/jessica joe.png',
     },
     {
         userName: 'Robin James',
-        avatar: '/users-avatar/robin james.png',
+        avatar: '/images/users-avatar/robin james.png',
     },
     {
         userName: 'Sofia Havertz2',
-        avatar: '/users-avatar/sofia havertz.png',
+        avatar: '/images/users-avatar/sofia havertz.png',
     },
     {
         userName: 'Nicola Jensen2',
-        avatar: '/users-avatar/nicolas jensen.png',
+        avatar: '/images/users-avatar/nicolas jensen.png',
     },
     {
         userName: 'Ashley Lee2',
-        avatar: '/users-avatar/ashley lee.png',
+        avatar: '/images/users-avatar/ashley lee.png',
     },
     {
         userName: 'Jessica Joe2',
-        avatar: '/users-avatar/jessica joe.png',
+        avatar: '/images/users-avatar/jessica joe.png',
     },
     {
         userName: 'Robin James2',
-        avatar: '/users-avatar/robin james.png',
+        avatar: '/images/users-avatar/robin james.png',
     },
     {
         userName: 'Sofia Havertz3',
-        avatar: '/users-avatar/sofia havertz.png',
+        avatar: '/images/users-avatar/sofia havertz.png',
     },
     {
         userName: 'Nicola Jensen3',
-        avatar: '/users-avatar/nicolas jensen.png',
+        avatar: '/images/users-avatar/nicolas jensen.png',
     },
     {
         userName: 'Ashley Lee3',
-        avatar: '/users-avatar/ashley lee.png',
+        avatar: '/images/users-avatar/ashley lee.png',
     },
     {
         userName: 'Jessica Joe3',
-        avatar: '/users-avatar/jessica joe.png',
+        avatar: '/images/users-avatar/jessica joe.png',
     },
     {
         userName: 'Robin James3',
-        avatar: '/users-avatar/robin james.png',
+        avatar: '/images/users-avatar/robin james.png',
     },
 ]
