56868320d14cf5a0ba5ae6b04b648eebfea6ccf8
najijrwan <najijrwanyt@gmail.com>
2026-08-14 15:39:34 +0300

    refactor(data): use PRODUCTS to store user's wishlist products data

--- src/data/account/accountWishlistProducts.js
+++ src/data/account/accountWishlistProducts.js
@@ -1,30 +1,33 @@
+import { PRODUCTS } from "@data";
+import { finalPrice } from '@utils';
+
 export const ACCOUNT_WISHLIST_PRODUCTS = [
     {
-        id: 0,
-        name: 'tray table',
-        color: 'black',
-        price: 19.19,
-        image: "src/assets/images/product image 1 black.png",
+        id: PRODUCTS.TRAY_TABLE.id,
+        label: PRODUCTS.TRAY_TABLE.label,
+        image: PRODUCTS.TRAY_TABLE.variants.BLACK.image,
+        color: PRODUCTS.TRAY_TABLE.variants.BLACK.color,
+        price: finalPrice(PRODUCTS.TRAY_TABLE.price, PRODUCTS.TRAY_TABLE.discountPercentage),
     },
     {
-        id: 1,
-        name: 'sofa',
-        color: 'beige',
-        price: 345,
-        image: "src/assets/images/product image 13.png",
+        id: PRODUCTS.COZY_SOFA.id,
+        label: PRODUCTS.COZY_SOFA.label,
+        image: PRODUCTS.COZY_SOFA.variants.BEIGE.image,
+        color: PRODUCTS.COZY_SOFA.variants.BEIGE.color,
+        price: finalPrice(PRODUCTS.COZY_SOFA.price, PRODUCTS.COZY_SOFA.discountPercentage),
     },
     {
-        id: 2,
-        name: 'bamboo basket',
-        color: 'beige',
-        price: 80.80,
-        image: "src/assets/images/product image 9.png",
+        id: PRODUCTS.BAMBOO_BASKET.id,
+        label: PRODUCTS.BAMBOO_BASKET.label,
+        image: PRODUCTS.BAMBOO_BASKET.variants.BEIGE.image,
+        color: PRODUCTS.BAMBOO_BASKET.variants.BEIGE.color,
+        price: finalPrice(PRODUCTS.BAMBOO_BASKET.price, PRODUCTS.BAMBOO_BASKET.discountPercentage),
     },
     {
-        id: 3,
-        name: 'pillow',
-        color: 'beige',
-        price: 80.80,
-        image: "src/assets/images/product image 15.png",
-    }
+        id: PRODUCTS.LIGHT_BEIGE_PILLOW.id,
+        label: PRODUCTS.LIGHT_BEIGE_PILLOW.label,
+        image: PRODUCTS.LIGHT_BEIGE_PILLOW.variants.BEIGE.image,
+        color: PRODUCTS.LIGHT_BEIGE_PILLOW.variants.BEIGE.color,
+        price: finalPrice(PRODUCTS.LIGHT_BEIGE_PILLOW.price, PRODUCTS.LIGHT_BEIGE_PILLOW.discountPercentage),
+    },
 ];
