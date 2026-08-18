41e708485346bc413df8b27edd8c65922fe89cae
najijrwan <najijrwanyt@gmail.com>
2026-08-18 16:39:53 +0300

    feat(account): use current user data to render avatar and username

--- src/components/sections/account/AccountMenu.jsx
+++ src/components/sections/account/AccountMenu.jsx
@@ -1,14 +1,16 @@
+import { useAuth } from '@contexts';
 import { useBreakpoint } from '@hooks';
 import { Icon, Dropdown } from '@ui';
 import { ACCOUNT_MENU } from '@data';
 
 const AccountMenu = ({ activeItem, setActiveItem }) => {
+    const { currentUser } = useAuth();
     const { isMobile } = useBreakpoint();
 
     return (
         <div className='h-fit px-4 py-10 bg-n2100 rounded-lg'>
             <div className='relative size-[82px] mx-auto rounded-full'>
-                <img src="/images/users-avatar/sofia havertz.png" alt="user profile" />
+                <img src={currentUser.avatar} alt="user profile" />
                 <div
                     className='
                     absolute bottom-0 right-0
@@ -20,7 +22,9 @@ const AccountMenu = ({ activeItem, setActiveItem }) => {
                     <Icon name="Camera" spanClassName="size-4 text-n1100" iconClassName="w-[13.33px] h-[12px]" />
                 </div>
             </div>
-            <p className='w-fit mx-auto mb-10 text-black body-1-semi'>Sofia Havertz</p>
+            <p className='w-fit mx-auto mb-10 text-black body-1-semi'>
+                {currentUser.username}
+            </p>
             {isMobile
                 ? (
                     <Dropdown
