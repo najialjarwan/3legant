255eb9d9acb297a9f3f0254ac5a3dd6981feb301
najijrwan <najijrwanyt@gmail.com>
2026-08-18 16:58:15 +0300

    style(ui): uncapitalize placeholder text value

--- src/components/ui/input/Input.jsx
+++ src/components/ui/input/Input.jsx
@@ -10,7 +10,7 @@ const Input = ({ type, required = false, label = '', placeholder, caption = '' }
                     w-full h-10 px-4
                     body-2 text-n4100
                     border border-black-300 rounded-md
-                    placeholder:capitalize placeholder:text-n4100 outline-0'
+                    placeholder:text-n4100 outline-0'
             />
             {caption && (
                 <p className="text-n4100 text-xs/5 tracking-normal font-inter italic">
