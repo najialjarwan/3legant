da461c47d2cd9a01b96779367f4cedb4193fe4f2
najijrwan <najijrwanyt@gmail.com>
2026-08-23 18:37:52 +0300

    fix(product): unexport product provider

--- src/components/sections/product/index.js
+++ src/components/sections/product/index.js
@@ -1,13 +1,8 @@
-export * from './product-context/ProductContext';
-
-
 export { default as ProductLayout } from './product-layout/ProductLayout';
-
 
 export { default as ProductLoop } from './ProductLoop/ProductLoop';
 export { default as ProductInfo } from './ProductLoop/product-info/ProductInfo';
 export { default as ProductMedia } from './ProductLoop/product-media/ProductMedia';
-
 
 export { default as ProductTabs } from './ProductTabs/ProductTabs';
 export { default as ProductReviews } from './ProductTabs/product-reviews/ProductReviews';
